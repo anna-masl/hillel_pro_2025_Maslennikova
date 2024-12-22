f25caac19a0de28af8f9f0857e2389f4ee3de1e5
anna-masl <anna.maslinnikova@gmail.com>
2024-12-22 22:56:59 +0200

    program

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
 
 public class Main {
+    private static final double CONV_K = 2.20462;
     public static void main(String[] args) {
-        System.out.println("Hello world__!");
+        System.out.println("Convert App.");
+        double kgs = 10;
+        double pnds = 15;
+        double pounds = convKgsToPounds(kgs);
+        double kilos = convPoundsToKgs(pnds);
+        System.out.println("Result is " + pounds + " pounds and " + kilos + " kgs.");
+
+    }
+
+    private static double convKgsToPounds(double kgs) {
+        return kgs * CONV_K;
+    }
+
+    private static double convPoundsToKgs(double pnds) {
+        return pnds / CONV_K;
     }
 }
